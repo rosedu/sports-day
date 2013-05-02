022ac250b8b00eba163907e6892a797cee5eaa5f
Alex Eftimie <alex@eftimie.ro>
2013-05-02 15:40:17 +0300

    Add a html and a fabfile

---
+++ fabfile.py
@@ -0,0 +1,11 @@
+from fabric.api import *
+from fabric.contrib.project import rsync_project
+
+env['use_ssh_config'] = True
+env['hosts'] = ['events@rosedu.org']
+env['target_directory'] = 'public_html/sports-day'
+
+
+@task
+def deploy():
+    rsync_project(env['target_directory'], 'html', delete=True)
